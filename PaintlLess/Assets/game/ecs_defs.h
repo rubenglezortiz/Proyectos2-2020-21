e67e20e4250bd0db9baa9915c22a2db53484b041
Rubén González Ortiz <rubgon08@ucm.es>
2021-03-12 15:39:21 +0100

    plantilla x64
    He sacado la plantilla de alberto, en caso de fallo deshacemos commit

---
+++ PaintlLess/Assets/game/ecs_defs.h
@@ -0,0 +1,47 @@
+// This file is part of the course TPV2@UCM - Samir Genaim
+
+#pragma once
+
+// components -- forward declaration, i.e., assume they are defined somewhere
+//class Transform;
+//class Rectangle;
+//class KeyBoardCtrl;
+//class Bounce;
+//class Image;
+//class Rotate;
+//class DeAcceleration;
+//class Health;
+//class ShowAtOpposieSide;
+//class Follow;
+
+#define _CMPS_LIST_  \
+	/*Transform,\
+	Rectangle,\
+	KeyBoardCtrl,\
+	Image, \
+	Rotate, \
+	Bounce,\
+	DeAcceleration,\
+	Health,\
+	ShowAtOpposieSide,\
+	Follow*/
+
+// groups
+
+struct Group_1;
+struct Lives;
+struct Asteroid_grp;
+struct Bullet_grp;
+
+#define _GRPS_LIST_ \
+	Group_1, \
+	Lives ,\
+	Asteroid_grp,\
+	Bullet_grp
+
+
+// handlers
+struct Hdlr_1;
+#define _HDLRS_LIST_ Hdlr_1
+
+
