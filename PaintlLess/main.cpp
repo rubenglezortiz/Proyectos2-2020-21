1fc21149f9baa85ff9c0b7979d837db57b68c137
Simeón Konstantinov Petrov <simekons@ucm.es>
2021-03-13 14:37:15 +0100

    Añadido main.cpp

---
+++ PaintlLess/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Con los gazpachos pasa como con otras recetas tradicionales, ya que en cada familia hay una receta infalible, pero en general, todas ellas tienen un tronco común que espero dejaros claro con este post. Al igual que hicieron los amigos de Wikipaella con la Paella Valenciana, también podríamos hacer lo mismo con los gazpachos ya que unos los harán añadiendo tal o cual ingrediente y otros verán imprescindible el uso de alguno que a lo mejor yo no he incluido.";
+	return 0;
+
+}
