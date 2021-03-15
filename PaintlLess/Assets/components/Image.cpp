9c0af217465614da4d1104cb20ddbdc972e2966a
Rubén González Ortiz <rubgon08@ucm.es>
2021-03-15 16:23:16 +0100

    Cambio de textura de casilla al pasar el ratón por encima

--- PaintlLess/Assets/components/Image.cpp
+++ PaintlLess/Assets/components/Image.cpp
@@ -4,3 +4,12 @@ void Image::render()  {
 	SDL_Rect dest = build_sdlrect(tr_->getPos(), tr_->getW(), tr_->getH());
 	tex_->render(src_, dest, tr_->getRot());
 }
+
+void Image::shiny() {
+	if (src_.x < 3 * src_.w) //el 3 está cableado por código
+		src_.x += src_.w;
+}
+
+void Image::dark() {
+	src_.x = currFrame;
+}
