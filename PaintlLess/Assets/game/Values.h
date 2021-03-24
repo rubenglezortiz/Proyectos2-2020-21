4d15428a140f3652414a97ab5f8e474bd8032944
Manuel Adeliño <madelino@ucm.es>
2021-03-24 16:37:18 +0100

    values.h creado

---
+++ PaintlLess/Assets/game/Values.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+enum Unit { Alquimista, Arquitecta, Bomba, Cazador, Druida, Esqueleto, Golem, Kirin, Lobo, Monaguillo, Picara, Tanque, Vikingo};
+
+typedef unsigned int uint;
+
+namespace UnitInfo {
+enum TipoCarta {Ataque, Defensa, Utilidad};
+
+
+	const uint numUnidades = 13;
+	const uint Ataque = {1};
+	
+	const uint Movimiento[numUnidades];
+	const uint Cooldown = {3};
+	const uint Mana = {3};
+	const uint Vida = {2};
+	const TipoCarta tipoCarta = {TipoCarta::Ataque};
+
+
+
+	/*Ataque[Arquitecta] =1;
+	Ataque[Bomba] = 0;
+	Ataque[Cazador] = 1;
+	Ataque[Druida] = 1;
+	Ataque[Esqueleto] = 1;
+	Ataque[Golem] = 1;
+	Ataque[Kirin] = 1;
+	Ataque[Lobo] = 0;
+	Ataque[Monaguillo] = ;
+	Ataque[Picara] = ;
+	Ataque[Tanque] = ;
+	Ataque[Vikingo] = ;*/
+
+
+
+			
+		
+
+
+}
