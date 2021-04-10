39fb89c2d3f53a772e1adebffc2f5a2fba1375d5
Rioni <nrosa01@ucm.es>
2021-04-10 17:57:03 +0200

    Arreglado problema de eficiencia en el paso de parametros de lerpFunct

--- PaintlLess/Assets/game/LerpingFunctions.h
+++ PaintlLess/Assets/game/LerpingFunctions.h
@@ -1,7 +1,7 @@
 #pragma once
 
 enum LerpType { Linear, EaseIn, EaseOut };
-typedef float (*LerpFunction)(float args);
+typedef float (*LerpFunction)(const float &lerpTime);
 
 namespace LerpFuncts
 {
@@ -9,31 +9,27 @@ namespace LerpFuncts
 	//Todos los métodos que incluyen p_ son privados y no deben ser llamados desde fuera
 	//Todos los métodos que incluten por f_, son LerpFunctions
 
-	inline float p_f_Linear(float t)  { return t;     }	 // no easing, no acceleration
-	inline float p_f_Flip(float x)    { return 1 - x; }
-	inline float p_f_EaseIn(float t)  { return t * t; }	 // decelerating to zero velocity
-	inline float p_f_EaseOut(float t) { return p_f_Flip(p_f_EaseIn(p_f_Flip(t))); }
+	inline float p_f_Linear (const float &t)  { return t;     }	 // no easing, no acceleration
+	inline float p_f_Flip   (const float &x)  { return 1 - x; }
+	inline float p_f_EaseIn (const float &t)  { return t * t; }	 // decelerating to zero velocity
+	inline float p_f_EaseOut(const float &t)  { return p_f_Flip(p_f_EaseIn(p_f_Flip(t))); }
 
-	/*
-	 * Devuelve un número entre v0 y v1 en función de t y la función de lerping
-	 */
-	inline float p_CustomLerp(float v0, float v1, float t, LerpFunction function = p_f_Linear) 
+	//Devuelve un número entre v0 y v1 en función de t y la función de lerping
+	inline float p_CustomLerp(const float &v0, const float &v1, const float &t, LerpFunction function = p_f_Linear) 
 	{
-		t = function(t);
-		return (1 - t) * v0 + t * v1;
+		float auxT = function(t);
+		return (1 - auxT) * v0 + auxT * v1;
 	}
 
-	/*
-	 * Este es el método a llamar para usar el lerping
-	 */
-	inline float Lerp(float v0, float v1, float t, LerpType lerpType = LerpType::EaseOut) 
+	//Este es el método a llamar para usar el lerping
+	inline float Lerp(const float &v0,const float &v1, const float &t, LerpType lerpType = LerpType::EaseOut) 
 	{
 		switch (lerpType)
 		{
 			case Linear:	  return p_CustomLerp(v0, v1, t, p_f_Linear);  break;
 			case EaseIn:	  return p_CustomLerp(v0, v1, t, p_f_EaseIn);  break;
 			case EaseOut:	  return p_CustomLerp(v0, v1, t, p_f_EaseOut); break;
-			default:		  throw("Algo fue mal"); break;
+			default:		  throw("No se encontró la función de lerp");  break;
 		}
 	}
 }
