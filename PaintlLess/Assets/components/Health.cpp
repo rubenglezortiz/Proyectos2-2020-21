0f6addc41d7419984dfb029c3bf814996d57a276
simekons <simekons@ucm.es>
2021-05-24 13:12:18 +0200

    Las vidas ya no se solapan.

--- PaintlLess/Assets/components/Health.cpp
+++ PaintlLess/Assets/components/Health.cpp
@@ -52,42 +52,35 @@ void Health::kill(GameMap* mapa, Entity* entity_)
 
 void Health::render() {
 	if (lives < 50) {
-		if (entity_->hasGroup<Equipo_Rojo>())
-		{
-			SDL_Rect src;
-			src.x = 0; src.y = 0; src.w = tex->width(); src.h = tex->height();
+		SDL_Rect src;
+		src.x = 0; src.y = 0; src.w = tex->width(); src.h = tex->height();
 
-			Vector2D pos = tr->getPos();
+		Vector2D pos = tr->getPos();
 
-			SDL_Rect dest;
-			dest.x = pos.getX() + mapa->getCellWidth() - tr->getW() / 6; dest.y = pos.getY() + mapa->getCellHeight() - tr->getH() / 2;
-			dest.w = tr->getW() / 6; dest.h = tr->getH() / 5;
-
-			for (int i = 0; i < lives; ++i)
-			{
-				tex->render(src, dest, 0);
-				//Cada vez que pinta uno cambia la posici�n x del siguiente
-				dest.x = dest.x - dest.w;
-			}
+		SDL_Rect dest;
+		dest.w = tr->getW() / 6; dest.h = tr->getH() / 5;
+		dest.x = pos.getX() + tr->getW() / 2; dest.y = pos.getY() - 50;
+		switch (lives) {
+		case 1:
+			dest.x -= dest.w / 2;
+			break;
+		case 2:
+			dest.x -= dest.w;
+			break;
+		case 3:
+			dest.x -= (1.5) * dest.w;
+			break;
+		case 4:
+			dest.x -= 2 * dest.w;
+			break;
 		}
-		else
-		{
-			SDL_Rect src;
-			src.x = 0; src.y = 0; src.w = tex->width(); src.h = tex->height();
 
-			Vector2D pos = tr->getPos();
-
-			SDL_Rect dest;
-			dest.x = pos.getX() - mapa->getCellWidth() + tr->getW(); dest.y = pos.getY() + mapa->getCellHeight() - tr->getH() / 2;
-			dest.w = tr->getW() / 6; dest.h = tr->getH() / 5;
-
-			for (int i = 0; i < lives; ++i)
-			{
-				tex->render(src, dest, 0);
-				//Cada vez que pinta uno cambia la posici�n x del siguiente
-				dest.x = dest.x + dest.w;
-			}
+		for (int i = 0; i < lives; ++i) {
+			tex->render(src, dest, 0);
+			//Cada vez que pinta uno cambia la posici�n x del siguiente
+			dest.x = dest.x + dest.w;
 		}
+
 	}
 }
 
